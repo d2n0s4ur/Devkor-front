4902a959c57bfcf62673cfa1171829ec62d93b9e
d2n0s4ur <njh0625@gmail.com>
2022-05-16 20:14:22 +0900

    update: Front-end 3rd Seminar

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,21 +4,21 @@ import React, { useState } from 'react';
 
 function App(){
   let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남 우동맛집', '파이썬 독학'] );
-  let [modal, setModal] = useState(false);
+  let [modal, setModal] = useState([false, false, false]);
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
       {
-        글제목.map((title)  => {
+        글제목.map((title, index)  => {
           return (
             <div className="list">
-              <h4>{title}</h4>
+              <h4>{글제목[index]}</h4>
               <p>2월 17일 발행</p>
-              <button onClick={ ()=>{ setModal(!modal) } }> {title} </button>
+              <button onClick={ ()=>{let copy = [...modal]; copy[index] = !modal[index]; setModal(copy); } }> {글제목[index]} </button>
               {
-                modal === true ? <Modal></Modal> : null
+                modal[index] == true ? <Modal title={글제목[index]}></Modal> : null
               }
             </div>
           )
@@ -34,10 +34,10 @@ function App(){
   )
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-        <h4>글제목</h4>
+        <h4>{props.title}</h4>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
