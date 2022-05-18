85cd7c2f2b0a3b5165e5482a5484ef793e1170ba
d2n0s4ur <njh0625@gmail.com>
2022-05-18 14:18:53 +0900

    update: Front end assignment

--- blog/src/App.js
+++ blog/src/App.js
@@ -18,18 +18,12 @@ function App(){
               <p>2월 17일 발행</p>
               <button onClick={ ()=>{let copy = [...modal]; copy[index] = !modal[index]; setModal(copy); } }> {글제목[index]} </button>
               {
-                modal[index] == true ? <Modal title={글제목[index]}></Modal> : null
+                modal[index] == true ? <Modal title={글제목} titlechange={글제목변경} index={index}></Modal> : null
               }
             </div>
           )
         })
       }
-      <button onClick={ () => {
-        let copy = [...글제목];
-        copy[0] = '여자코트 추천';
-        글제목변경(copy)
-        }
-      }> 수정버튼 </button>
     </div>
   )
 }
@@ -37,9 +31,15 @@ function App(){
 function Modal(props){
   return (
     <div className="modal">
-        <h4>{props.title}</h4>
+        <h4>{props.title[props.index]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={ () => {
+        let copy = [...props.title];
+        copy[0] = '여자코트 추천';
+        props.titlechange(copy);
+        }
+      }> 수정버튼 </button>
       </div>
   )
 }
