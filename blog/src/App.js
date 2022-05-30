5315c75a301e33b03875a3712f68c1d8dc1e7bb4
d2n0s4ur <njh0625@gmail.com>
2022-05-30 17:17:29 +0900

    update:practice4

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 function App(){
   let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남 우동맛집', '파이썬 독학'] );
   let [modal, setModal] = useState([false, false, false]);
+  let [input, setinput] = useState('');
   return (
     <div className="App">
       <div className="black-nav">
@@ -20,10 +21,18 @@ function App(){
               {
                 modal[index] == true ? <Modal title={글제목} titlechange={글제목변경} index={index}></Modal> : null
               }
+              <button onClick={ ()=>{
+                let copy = [...글제목]; copy.splice(index,1); 글제목변경(copy);
+                let copymodal = [...modal]; modal.splice(index, 1); setModal(copymodal);
+                } }> 삭제 </button>
             </div>
           )
         })
       }
+      <input onChange = {(e)=> {setinput(e.target.value)}}/>
+      <button onClick={()=>{let copy = [...글제목]; copy.unshift(input); 글제목변경(copy);
+        let copymodal = [...modal]; modal.unshift(false); setModal(copymodal);
+      }}>추가</button>
     </div>
   )
 }
